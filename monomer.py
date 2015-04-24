2a9255095e87b436f1087de1e53129d15c986c71
oliviaAB <olivia.angelinbonnet@gmail.com>
2015-04-24 10:43:59 +0200

    Create class fixed obstacles

--- monomer.py
+++ monomer.py
@@ -1,8 +1,17 @@
-##from __future__ import division
-##from scipy import * 
-##from pylab import *
-##from scipy.integrate import odeint
-##import os, time
+#------------------------------------------------------------------------------
+#                                 Projet 3BIM
+#                          Cytoskeleton modelisation
+#              ANGELIN-BONNET Olivia, GURJAO CARINO, PIAT Alexis
+#------------------------------------------------------------------------------
+
+# Hypotheses:
+# 1: aux bords de la cellule, les monomeres subissent du "vent": le bord est plus
+#     visqueux, ils ralentissent tout en etant devies de leur trajectoire avec
+#     une force proportionnelle a leur proximite a la membrane
+
+# 2: Lorsqu'un monomere percute un obstacle (cad lorsqu'il est suffisamment proche,
+#     ils rebondissent l'un contre l'autre
+
 
 from Tkinter import *
 
@@ -14,12 +23,16 @@ SPEED=20
 TIME=0.1 #0.01
 
 NB_MONO=10 #number of monomers in the cell
-NB_OBS=5 #number of obstacles in the cell
+NB_OBS=1 #number of obstacles in the cell
+NB_FIX=1 #number of fixed obstacles in the cell
 
 R=200
 RAYON=5
+RAYON_FIX=20
 
-CONTACT_OBS=10
+CONTACT_MONO=5
+CONTACT_OBS=7
+CONTACT_FIX=22
 
 
 class monomer:
@@ -60,10 +73,10 @@ class monomer:
 ##        self.r=math.sqrt(self.x*self.x+self.y*self.y)
 ##        self.theta=math.atan2(self.y,self.x)
 
-    def near_obs(self, obstacle):
+    def near(self, obstacle, contact):
         res=0
         norm=math.sqrt((self.x-obstacle.x)*(self.x-obstacle.x)+(self.y-obstacle.y)*(self.y-obstacle.y))
-        if norm<CONTACT_OBS:
+        if norm<contact:
             res=1
         return res
 
@@ -107,25 +120,42 @@ class monomer:
                     self.v2=self.v2-braking
 
 
-    def move(self, obstacles):
+    def move(self, obstacles, monomers,fixed,num):
         obs=0
         v1=0
         v2=0
 
         for i in xrange(NB_OBS):
-            if self.near_obs(obstacles[i])==1:
+            if self.near(obstacles[i], CONTACT_OBS)==1:
                 v1=v1+obstacles[i].v1-self.v1
                 v2=v2+obstacles[i].v2-self.v2
                 obs+=1
 
-                obstacles[i].v1=self.v1-obstacles[i].v1
-                obstacles[i].v2=self.v2-obstacles[i].v2
+        for y in xrange(NB_MONO):
+            if y!=num:
+                if self.near(monomers[y], CONTACT_MONO)==1:
+                    v1=v1+monomers[y].v1-self.v1
+                    v2=v2+monomers[y].v2-self.v2
+                    obs+=1
+
+        fix=0
+        for j in xrange(NB_FIX):
+            if self.near(fixed[j],CONTACT_FIX)==1:
+                self.v1=-self.v1
+                self.v2=-self.v2
+                fix=1
+                    
                 
         if obs==0:
             self.move_random()
         else:
             self.v1=v1/obs
             self.v2=v2/obs
+            norm=math.sqrt(self.v1*self.v1+self.v2*self.v2)
+            self.v1=self.v1*(SPEED+10)/norm
+            self.v2=self.v2*(SPEED+10)/norm
+
+        if fix!=0:
             norm=math.sqrt(self.v1*self.v1+self.v2*self.v2)
             self.v1=self.v1*(SPEED+10)/norm
             self.v2=self.v2*(SPEED+10)/norm
@@ -178,7 +208,14 @@ class obstacle:
 ##        self.r=math.sqrt(self.x*self.x+self.y*self.y)
 ##        self.theta=math.atan2(self.y,self.x)
 
-    def move(self):
+    def near(self, obstacle, contact):
+        res=0
+        norm=math.sqrt((self.x-obstacle.x)*(self.x-obstacle.x)+(self.y-obstacle.y)*(self.y-obstacle.y))
+        if norm<contact:
+            res=1
+        return res
+
+    def move_random(self):
 
         #coordonates of moving vector between -1 and 1
         self.v1=self.v1+(random.random()*2-1)*5
@@ -189,7 +226,7 @@ class obstacle:
         self.v1=self.v1*SPEED/norm
         self.v2=self.v2*SPEED/norm
 
-        #wind
+    def wind(self):
         if self.r>(R-50):
             braking=(50-(R-self.r))/5
             #print braking
@@ -218,16 +255,68 @@ class obstacle:
                     self.v2=self.v2-braking
 
 
-        #coordonates updated
-        #self.r=self.r+TIME*self.v1
-        #self.theta=self.theta+TIME*self.v2
+    def move(self, obstacles, monomers,fixed,num):
+        obs=0
+        v1=0
+        v2=0
+
+        for i in xrange(NB_OBS):
+            if i!=num:
+                if self.near(obstacles[i], CONTACT_OBS)==1:
+                    v1=v1+obstacles[i].v1-self.v1
+                    v2=v2+obstacles[i].v2-self.v2
+                    obs+=1
+
+        for y in xrange(NB_MONO):
+            if self.near(monomers[y],CONTACT_MONO)==1:
+                v1=v1+monomers[y].v1-self.v1
+                v2=v2+monomers[y].v2-self.v2
+                obs+=1
+
+        for j in xrange(NB_FIX):
+            if self.near(fixed[j],CONTACT_FIX)==1:
+                self.v1=-self.v1
+                self.v2=-self.v2
+                
+        if obs==0:
+            self.move_random()
+        else:
+            self.v1=v1/obs
+            self.v2=v2/obs
+            norm=math.sqrt(self.v1*self.v1+self.v2*self.v2)
+            self.v1=self.v1*(SPEED+10)/norm
+            self.v2=self.v2*(SPEED+10)/norm
+
+        self.wind()
+
         self.x=self.x+TIME*self.v1
         self.y=self.y+TIME*self.v2
 
-        #print self.v1, self.v2
+        self.update_pol()                
 
-        #self.update_cart()
-        self.update_pol()
+#-----------------------------------------------------------------------------
+
+class obstacle_fixed:
+
+    def __init__(self):
+        
+        #polar coordinates initialized randomly
+        self.r=random.random()*R
+        self.theta=random.random()*math.pi-(math.pi/2)
+
+        #cartesian coordinates updated
+        self.x=self.r*math.cos(self.theta)+R
+        self.y=self.r*math.sin(self.theta)+R
+
+    def __repr__(self):
+        print self.r, self.theta
+        return str(self.x)+","+str(self.y)
+
+    def get_coordcart(self):
+        return(self.x,self.y)
+
+    def get_coordpol(self):
+        return(self.r,self.theta)
 
 #-----------------------------------------------------------------------------
 
@@ -249,11 +338,15 @@ class boid:
         self.monomers=[monomer() for i in xrange(NB_MONO)]
         #table of obstacles
         self.obstacles=[obstacle() for i in xrange(NB_OBS)]
+        #table of fixed obstacles
+        self.fixed=[obstacle_fixed() for i in xrange(NB_FIX)]
 
         #table of circles to draw for each monomer
         self.points_mono=[self.canevas.create_oval(self.monomers[i].x-RAYON,self.monomers[i].y-RAYON,self.monomers[i].x+RAYON,self.monomers[i].y+RAYON,width=1,outline='blue',fill='blue') for i in xrange(NB_MONO)]
         #table of circles to draw for each obstacle
         self.points_obs=[self.canevas.create_oval(self.obstacles[i].x-RAYON,self.obstacles[i].y-RAYON,self.obstacles[i].x+RAYON,self.obstacles[i].y+RAYON,width=1,outline='green',fill='green') for i in xrange(NB_OBS)]
+        #table of circles to draw for each fixed obstacle
+        self.points_fix=[self.canevas.create_oval(self.fixed[i].x-RAYON_FIX,self.fixed[i].y-RAYON_FIX,self.fixed[i].x+RAYON_FIX,self.fixed[i].y+RAYON_FIX,width=1,outline='red',fill='red') for i in xrange(NB_FIX)]
 
 
     def __repr__(self):
@@ -263,10 +356,12 @@ class boid:
 
     def move(self):
         for i in xrange(NB_OBS):
-            self.obstacles[i].move()
+            self.obstacles[i].move(self.obstacles,self.monomers,self.fixed,i)
+            #print "OBS", self.obstacles[i].v1,self.obstacles[i].v2
             
         for i in xrange(NB_MONO):
-            self.monomers[i].move(self.obstacles)
+            self.monomers[i].move(self.obstacles,self.monomers,self.fixed, i)
+            #print "MONO", self.monomers[i].v1,self.monomers[i].v2
 
     def draw(self):
         self.move()
